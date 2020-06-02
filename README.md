42ea121bcfc25043ca915d17085ee2fa0050bd93
Tyler Rule <tylerwrule@gmail.com>
2020-06-02 14:09:06 -0700

    updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # htmlToCSVGenerator
+This program requires the installation of Beautiful Soup and Requests
+To install:
+	pip install beautifulsoup4
+	pip install requests
--- csvScript.py
+++ csvScript.py
@@ -1,17 +1,33 @@
 import sys, os
+import requests
+from bs4 import BeautifulSoup
 
 def main(filePath):
 	directory = filePath
 
-	dir_path = os.getcwd() + "/" + directory
+	# Gets pwd of current directory and adds directory to it
+	dir_path = os.getcwd() + "/" + directory + "/"
+	# Creates a list of everything in the directory
 	dir_list = os.listdir(directory)
 
-	for file in dir_list:
-		printContent(dir_path + "/" + file)
+	# iterates through every file in the directory
+	for file in dir_list[1:]:
+		printContent(dir_path + file)
 
+#prints content of specific file
 def printContent(fileArg):
 	with open(fileArg, 'r') as f:
 		f_contents = f.read()
 		print(f_contents)
 
+outFileName = sys.argv[1] + "CSV.csv"
+
+outFile = open(outFileName, 'w')
+outFile.write("dog")
+# Gets commandLine Arg
+# 
+# Folder containing all relevant files must be in the same 
+#	folder that the script is located
 main(sys.argv[1])
+outFile.write("cat\n")
+outFile.close()
