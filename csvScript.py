cc87c1e96d7ea9e227f0546619c87e5e7190be45
Tyler Rule <tylerwrule@gmail.com>
2020-06-02 12:29:56 -0700

    successfully opening and printing a single file given by a filepath

--- csvScript.py
+++ csvScript.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import sys
+
+def main(filePath):
+	print("Hello World")
+	print(filePath)
+	f = open("../../../Desktop/12.1.3_12.2.9/12.1.3_12.2.9_products.txt", "r")
+	print(f.read())
+
+main(sys.argv[1])
